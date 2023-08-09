94f9f2ad4a041ecb319b60fdd22f38968581b2b5
silvhua <silvhua@gmail.com>
2023-08-09 16:03:48 -0700

    print event data

--- src/ghl_webhook_lambda.py
+++ src/ghl_webhook_lambda.py
@@ -1,8 +1,24 @@
 import json
+import sys
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    
+    try:
+        # json_data = json.loads(event)
+        json_data = event
+
+        # return {
+        #     'statusCode': 200,
+        #     'body': json_data
+        # }
+        return json_data
+    except Exception as error:
+        exc_type, exc_obj, tb = sys.exc_info()
+        f = tb.tb_frame
+        lineno = tb.tb_lineno
+        filename = f.f_code.co_filename
+        print("An error occurred on line", lineno, "in", filename, ":", error)
+        return {
+            "statusCode": 500,
+            "body": json.dumps("Error processing data: " + str(error))
+        }
