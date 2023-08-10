b6db9fb0db252173508e0358d588b8f44343e3d1
silvhua <silvhua@gmail.com>
2023-08-09 18:38:16 -0700

    success receiving webhook & returning json payload

--- src/ghl_webhook_lambda.py
+++ src/ghl_webhook_lambda.py
@@ -5,13 +5,18 @@ def lambda_handler(event, context):
     
     try:
         # json_data = json.loads(event)
-        json_data = event
+        json_data = event["body"]
 
         # return {
         #     'statusCode': 200,
         #     'body': json_data
         # }
-        return json_data
+        print(json_data)
+        # return json_data
+        return {
+            "statusCode": 200,
+            "body": json_data.json()
+        }
     except Exception as error:
         exc_type, exc_obj, tb = sys.exc_info()
         f = tb.tb_frame
