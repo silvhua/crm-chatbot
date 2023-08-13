c1ca9b4d8dfd08ec17c0e3b339fd3f031426f6bc
silvhua <silvhua@gmail.com>
2023-08-12 21:30:09 -0700

    Save webhook data of certain types into DynamoDB

--- src/ghl_webhook_lambda.py
+++ src/ghl_webhook_lambda.py
@@ -1,21 +1,32 @@
 import json
 import sys
+import boto3
 
 def lambda_handler(event, context):
     
     try:
-        # json_data = json.loads(event)
-        json_data = event["body"]
-
-        # return {
-        #     'statusCode': 200,
-        #     'body': json_data
-        # }
-        print(json_data)
-        # return json_data
+        payload = event["body"].json()
+        relevant_types = [
+            'ContactCreate', 'ContactDelete', 'ContactDndUpdate', 
+            'InboundMessage', 'OutboundMessage'
+            ]
+        if payload['type'] in relevant_types:
+            table_name = 'ghlWebhooks'
+            dynamodb = boto3.client('dynamodb') # Initialize DynamoDB client
+            item_attributes = dict()
+            for key, value in payload.items():
+                if type(value) == str:
+                    item_attributes[key] = {'S': value}
+            dynamodb.put_item(
+                TableName=table_name,
+                Item=item_attributes
+            )
+            message = f'Data for {payload.get("type", "")} webhook saved to DynamoDB successfully.'
+        else:
+            message = f'No need to save data for {payload.get("type", "")} webhook.'
         return {
             "statusCode": 200,
-            "body": json_data.json()
+            "body": json.dumps(message)
         }
     except Exception as error:
         exc_type, exc_obj, tb = sys.exc_info()
