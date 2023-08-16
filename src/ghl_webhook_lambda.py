7586d5c87c9da0c1d7cbee9eb33b56096cb7cb75
silvhua <111714881+silvhua@users.noreply.github.com>
2023-08-16 14:04:58 -0700

    Made compatible w/ API Gateway; handle dict, list payloads
    - Updated scripts because payload from Gateway API needed to be decoded with json.loads().
    - Updated script to handle payload items of types dict and list.

--- src/ghl_webhook_lambda.py
+++ src/ghl_webhook_lambda.py
@@ -6,11 +6,16 @@ from datetime import datetime, timezone
 def lambda_handler(event, context):
     
     try:
-        payload = event["body"]
+        if type(event["body"]) == str:
+            payload = json.loads(event["body"])
+        else:
+            payload = event["body"]
+            
         relevant_types = [
             'ContactCreate', 'ContactDelete', 'ContactDndUpdate', 
             'InboundMessage', 'OutboundMessage'
             ]
+        print(f'Payload: {payload}')
         if payload['type'] in relevant_types:
             table_name = 'ghlWebhooks'
             dynamodb = boto3.client('dynamodb') # Initialize DynamoDB client
@@ -18,6 +23,15 @@ def lambda_handler(event, context):
             for key, value in payload.items():
                 if type(value) == str:
                     item_attributes[key] = {'S': value}
+                elif type(value) == bool:
+                    item_attributes[key] = {"BOOL": value}
+                elif type(value) == dict:
+                    item_attributes[key] = {"S": json.dumps(value)}
+                elif type(value) == list:
+                    value = ''.join([f'{item}, ' for item in value])
+                    item_attributes[key] = {"S": value}
+                else:
+                    print(f'Unable to save payload item {key} of type {value}')
             if payload['type'] in ['ContactDelete', 'ContactDndUpdate']:
                 timestamp = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
                 item_attributes['dateAdded'] = {'S': timestamp}
@@ -28,6 +42,7 @@ def lambda_handler(event, context):
             message = f'Data for {payload.get("type", "")} webhook saved to DynamoDB successfully.'
         else:
             message = f'No need to save data for {payload.get("type", "")} webhook.'
+        print(message)
         return {
             "statusCode": 200,
             "body": json.dumps(message)
