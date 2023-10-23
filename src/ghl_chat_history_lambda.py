87d07dfb6fb38f8f748a2d7829007cbab75542a0
silvhua <silvhua@gmail.com>
2023-10-22 17:09:58 -0700

    Modularized the `ghl_chat_history_lambda`: created `add_webhook_data_to_dynamodb` in separate py file

--- src/ghl_chat_history_lambda.py
+++ src/ghl_chat_history_lambda.py
@@ -5,49 +5,29 @@ from datetime import datetime, timezone
 from data_functions import *
 
 def lambda_handler(event, context):
-    
+    """
+    Add GHL message events to dynamodb table as chat history.
+    """
     try:
         if type(event["body"]) == str:
             payload = json.loads(event["body"])
         else:
             payload = event["body"]
             
-        contact_events = ['ContactCreate', 'ContactDelete', 'ContactDndUpdate']
         message_events = ['InboundMessage', 'OutboundMessage']
         print(f'Payload: {payload}')
-        if payload['type'] in message_events:
-            table_name = 'ghlWebhooks'
+        if payload['type'] in message_events :
+            table_name = 'ghlWebhooks' ############
             dynamodb = boto3.client('dynamodb') # Initialize DynamoDB client
-            item_attributes = dict()
             contact_data = query_dynamodb_table(
                 'SessionTable', payload['contactId'], key='SessionId'
                 )['Items']
             if len(contact_data) == 0:
-                for key, value in payload.items():
-                    if value == 'contactId':
-                            item_attributes['SessionId'] = {'S': value}
-                    elif type(value) == str:
-                        item_attributes[key] = {'S': value}
-                    elif type(value) == bool:
-                        item_attributes[key] = {"BOOL": value}
-                    elif type(value) == dict:
-                        item_attributes[key] = {"S": json.dumps(value)}
-                    elif type(value) == list:
-                        value = ''.join([f'{item}, ' for item in value])
-                        item_attributes[key] = {"S": value}
-                    else:
-                        print(f'Unable to save payload item {key} of type {value}')
-            if payload['type'] in ['ContactDelete', 'ContactDndUpdate']:
-                timestamp = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
-                item_attributes['dateAdded'] = {'S': timestamp}
-            dynamodb.put_item(
-                TableName=table_name,
-                Item=item_attributes
-            )
-            message = f'Data for {payload.get("type", "")} webhook saved to DynamoDB successfully.'
-        else:
-            message = f'No need to save data for {payload.get("type", "")} webhook.'
-        print(message)
+                message = add_webhook_data_to_dynamodb(
+                    payload, table_name, dynamodb, chat=True
+                    )
+            else:
+                pass
         return {
             "statusCode": 200,
             "body": json.dumps(message)
