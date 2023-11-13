688765b8ab59fd5962936f5ae26e00e20c18e6c0
silvhua <silvhua@gmail.com>
2023-11-12 17:35:11 -0800

    query_dynamodb_table: partition+sort keys, profile

--- src/data_functions.py
+++ src/data_functions.py
@@ -5,23 +5,37 @@ from datetime import datetime, timezone
 import sys
 import os
 
-def query_dynamodb_table(table_name, value, key='SessionId', region_name="us-west-2"):
+def query_dynamodb_table(
+    table_name, partition_key_value, sort_key_value='', partition_key='SessionId', sort_key=None, 
+    profile=None, region_name="us-west-2"
+    ):
     """
-    Query a Dynamodb table and print the results.
+    Query a Dynamodb table based on both the partition key and sort key.
     Parameters:
         table_name (str): The name of the DynamoDB table.
-        value (str): The value of the key.
-        key (str): The name of the key.
+        partition_key_value (str): The value of the partition key.
+        sort_key_value (str): The value of the sort key.
+        partition_key (str): The name of the partition key.
+        sort_key (str): The name of the sort key.
         region_name (str): The name of the AWS region.
 
     Returns:
         dict: The results of the query. The 'Items' key contains the table record.       
     From 2023-10-22 notebook.
     """
-    dynamodb = boto3.resource('dynamodb', region_name=region_name)
+    if profile:
+        session = boto3.Session(profile_name=profile)
+        dynamodb = session.resource('dynamodb', region_name=region_name)
+    else:
+        dynamodb = boto3.resource('dynamodb', region_name=region_name)
     table = dynamodb.Table(table_name)
+    if sort_key:
+        KeyConditionExpression = Key(partition_key).eq(partition_key_value) & Key(sort_key).eq(sort_key_value)
+    else:
+        KeyConditionExpression = Key(partition_key).eq(partition_key_value)
+    print(f'KeyConditionExpression: {KeyConditionExpression}')
     response = table.query(
-        KeyConditionExpression=Key(key).eq(value)
+        KeyConditionExpression=KeyConditionExpression,
     )
     return response
 
