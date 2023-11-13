d654693288fdc09f79f374867f7809143e3868e6
silvhua <silvhua@gmail.com>
2023-11-13 15:56:46 -0800

    consolidate all ghl endpoints into `ghl_request

--- src/ghl_requests.py
+++ src/ghl_requests.py
@@ -1,137 +1,103 @@
 import requests
 from datetime import datetime
 import json
+import sys
 
-def send_message(contactId, message_dict, location='SamLab'):
+def ghl_request(contactId, endpoint='createTask', text=None, payload=None, location='SamLab'):
     """
-    Send a message to a contact in GoHighLevel.
-    
+    Send a message to a contact in GoHighLevel or retrieve email history.
+
     Parameters:
     - contactId (str): Contact ID.
-    - message_dict (dict): Dictionary containing message parameters. Dictionary keys should be:
-        - type (str): 'Email' or 'SMS'.
-        - message (str): Message body.
-        - subject (str): Email subject line.
-        - emailFrom (str): Email sender as shown in the email.
+    - endpoint (str): API endpoint. Valid values are 'createTask', 'createNote', 'send_message', and 'getEmailHistory'.
+    - payload (dict): Dictionary containing the payload for the request.
+    - params_dict (dict): Dictionary containing additional parameters for the request.
+    - location (str): Location value for retrieving the authentication token.
 
-        Here is an example message_dict:
+    Example payload for sendMessage endpoint:
         
-            message_dict = {
+            payload = {
                 "type": "Email",
                 "message": f"Hi, me. This is a test message from the GHL API using Python at {timestamp} Pacific time",
                 "subject": "Testing GHL API with Python threadId",
                 "emailFrom": "Brian Quinlan <brian@ownitfit.com.au>",
                 "threadId": 'Y0ecBIIPiHa7bpZr616G'
             }
-    Returns:
-    - response_dict (dict): Dictionary containing the response from the API.
-    """
-    url = "https://services.leadconnectorhq.com/conversations/messages"
-    with open('../private/auth_token_response.json') as token_file:
-        token = json.load(token_file)[location]
-    type = message_dict['type']
-    message = message_dict['message']
-    subject = message_dict['subject']
-    threadId = message_dict.get('threadId', None)
-    print(f'Sending {type} messages with these parameters:')
-    print(message_dict)
-
-    payload = {
-        "type": type,
-        "contactId": contactId
-    }
-    if type == 'Email':
-        emailFrom = message_dict['emailFrom']
-        payload['emailFrom'] = emailFrom
-        payload['html'] = message
-        payload["subject"] = subject
-    elif type == 'SMS':
-        payload['message'] = message
-        
-    
-    if threadId:
-        payload['threadId'] = threadId
-    
-    headers = {
-        "Authorization": f"Bearer {token['access_token']}",
-        "Version": "2021-04-15",
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    }
-    response = requests.post(url, json=payload, headers=headers)
-    print(f'Status code {response.status_code}: {response.reason}')
-
-    return response.json()
-
-def get_email_history(contactId, message_dict, location='SamLab'):
-    """
-    """
-    url = f"https://services.leadconnectorhq.com/conversations/search?contactId={contactId}"
-    with open('../private/auth_token_response.json') as token_file:
-        token = json.load(token_file)[location]
-    
-    headers = {
-        "Authorization": f"Bearer {token['access_token']}",
-        "Version": "2021-04-15",
-        "Accept": "application/json"
-    }
-    response = requests.get(url, headers=headers)
-    print(f'Status code {response.status_code}: {response.reason}')
-
-    return response.json()
-
-from datetime import datetime
-def ghl_request(contactId, endpoint='createTask', payload=None, params_dict=None, location='SamLab'):
-    """
-    Send a message to a contact in GoHighLevel.
-    
-    Parameters:
-    - contactId (str): Contact ID.
 
     Returns:
     - response_dict (dict): Dictionary containing the response from the API.
     """
     url_root = 'https://services.leadconnectorhq.com/'
+    if payload:
+        print(f'input payload: {payload}')
 
     if endpoint == 'createNote':
         endpoint_url = f'contacts/{contactId}/notes'
         request_type = 'POST'
-        payload = {
-            'body': f"Send message to contact {contactId} {datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')}",
-            'userId': contactId
-        }
+        if payload == None:
+            payload = {}
+            payload['body'] = (f"Send message to contact {contactId} {datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')}" if text==None else text)
+            payload['userId'] = contactId
     elif endpoint == 'createTask':
         endpoint_url = f'contacts/{contactId}/tasks'
         request_type = 'POST'
-        payload = {
-            'title': f'Send message to contact {contactId}',
-            'dueDate': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
-            'completed': False
-        }
+        if payload == None:
+            payload = {}
+            payload['title'] = f'Send message to contact {contactId}' if text==None else text
+        payload['dueDate'] = payload.get('dueDate', datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'))
+        payload['completed'] = False
+    elif endpoint == 'sendMessage':
+        endpoint_url = f'conversations/messages'
+        request_type = 'POST'
+        payload["contactId"] = contactId
+        if payload['type'] == 'Email':
+            payload['html'] = payload['message']
+    elif endpoint == 'getEmailHistory':
+        endpoint_url = f'conversations/search?contactId={contactId}'
+        request_type = 'GET'
+        payload = None
+    else:
+        raise ValueError("Invalid endpoint value. Valid values are 'createTask', 'createNote', 'sendMessage', and 'getEmailHistory'.")
+
     url = f'{url_root}{endpoint_url}'
     with open('../private/auth_token_response.json') as token_file:
         token = json.load(token_file)[location]
-    
+
     headers = {
         "Authorization": f"Bearer {token['access_token']}",
         "Version": "2021-04-15",
         "Content-Type": "application/json",
         "Accept": "application/json"
     }
-    print(f'Sending {endpoint} messages with these parameters:')
-    if params_dict:
-        print(params_dict)
+
     if request_type == 'POST':
         response = requests.post(
             url, headers=headers, 
             json=payload if payload else None
-            )
-    else:
+        )
+    elif request_type == 'GET':
         response = requests.get(
             url, headers=headers, 
             json=payload if payload else None
-            )
-    print(f'Status code {response.status_code}: {response.reason}')
-    print(f'Status code {response.status_code}: {response.reason}')
+        )
+    else:
+        raise ValueError("Invalid request type. Valid values are 'POST' and 'GET'.")
 
-    return response.json()
+    print(f'Status code {response.status_code}: {response.reason}')
+    data = response.json()
+    try:
+        if endpoint == 'getEmailHistory':
+            email_timestamp = data['conversations'][0]['dateUpdated']/1000
+            utc_time = datetime.utcfromtimestamp(email_timestamp)
+            pacific_time = utc_time.replace(tzinfo=pytz.utc).astimezone(pytz.timezone('US/Pacific'))
+            email_timestamp_str = pacific_time.strftime('%Y-%m-%d %H:%M:%S')
+
+            print(f'Last email sent: {email_timestamp_str} Pacific time')
+            data['emailTimestamp_pacific'] = email_timestamp_str
+    except Exception as error:
+        exc_type, exc_obj, tb = sys.exc_info()
+        f = tb.tb_frame
+        lineno = tb.tb_lineno
+        filename = f.f_code.co_filename
+        print("An error occurred on line", lineno, "in", filename, ":", error)
+    return data
