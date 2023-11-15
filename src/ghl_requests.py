c9c9b70e1c9ccab7fa2fc7b33579d6d5bb608a3b
silvhua <silvhua@gmail.com>
2023-11-14 19:16:28 -0800

    add `refresh_token` function from app.py

--- src/ghl_requests.py
+++ src/ghl_requests.py
@@ -2,6 +2,55 @@ import requests
 from datetime import datetime
 import json
 import sys
+from urllib.parse import urlencode
+
+def refresh_token():
+    token_file_path = '../private/auth_token_response.json'
+
+    with open('../private/config.json') as config_file:
+        appConfig = json.load(config_file)
+
+    with open(token_file_path, 'r') as token_file:
+        tokens = json.load(token_file)
+
+    if 'SamLab' in tokens:
+        sam_lab_token = tokens['SamLab']
+    else:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({"error": "SamLab token not found in token_file."})
+        }
+
+    data = {
+        'client_id': appConfig["clientId"],
+        'client_secret': appConfig["clientSecret"],
+        'grant_type': 'refresh_token',
+        'refresh_token': sam_lab_token["refresh_token"],
+        'user_type': 'Location',
+        # 'redirect_uri': 'https://6r8pb7q836.execute-api.us-west-2.amazonaws.com/oauth/callback',
+        'redirect_uri': 'http://localhost:3000/oauth/callback'
+    }
+
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    response = requests.post('https://services.leadconnectorhq.com/oauth/token', data=urlencode(data), headers=headers)
+
+    if response.status_code == 200:
+        tokens['SamLab'] = response.json()
+        with open(token_file_path, 'w') as token_file:
+            json.dump(tokens, token_file)
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response.json())
+        }
+    else:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({"error": "Failed to fetch access token."})
+        }
 
 def ghl_request(contactId, endpoint='createTask', text=None, payload=None, location='SamLab'):
     """
