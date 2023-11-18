d8012d316400233305f61e2b22ff07d5530e57d1
silvhua <silvhua@gmail.com>
2023-11-18 10:51:06 -0800

    unable to properly load access token

--- src/ghl_requests.py
+++ src/ghl_requests.py
@@ -121,83 +121,101 @@ def ghl_request(contactId, endpoint='createTask', text=None, payload=None, locat
     url_root = 'https://services.leadconnectorhq.com/'
     if payload:
         print(f'input payload: {payload}')
-
-    if endpoint == 'createNote':
-        endpoint_url = f'contacts/{contactId}/notes'
-        request_type = 'POST'
-        if payload == None:
-            payload = {}
-            payload['body'] = (f"Reply to SMS (contactID {contactId}) {datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')}" if text==None else text)
-            payload['userId'] = contactId
-    elif endpoint == 'createTask':
-        endpoint_url = f'contacts/{contactId}/tasks'
-        request_type = 'POST'
-        if payload == None:
-            payload = {}
-            payload['title'] = f'Send message to contact {contactId}'
-            payload['body'] = text if text else f"Test task via GHL API at {datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')} Pacific time"
-        payload['dueDate'] = payload.get('dueDate', datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'))
-        payload['completed'] = False
-    elif endpoint == 'sendMessage':
-        endpoint_url = f'conversations/messages'
-        request_type = 'POST'
-        payload["contactId"] = contactId
-        if payload['type'] == 'Email':
-            payload['html'] = payload['message']
-    elif endpoint == 'getEmailHistory':
-        endpoint_url = f'conversations/search?contactId={contactId}'
-        request_type = 'GET'
-        payload = None
-    else:
-        raise ValueError("Invalid endpoint value. Valid values are 'createTask', 'createNote', 'sendMessage', and 'getEmailHistory'.")
-
-    url = f'{url_root}{endpoint_url}'
     try:
-        with open('private/auth_token_response.json') as token_file:
-            token = json.load(token_file)[location]
-    except:
-        s3 = boto3.client('s3')
-        response = s3.get_object(Bucket='ownitfit-silvhua', Key='auth_token_response.json')
-        token = json.loads(response['Body'].read().decode('utf-8'))[location]
+        if endpoint == 'createNote':
+            endpoint_url = f'contacts/{contactId}/notes'
+            request_type = 'POST'
+            if payload == None:
+                payload = {}
+                payload['body'] = (f"Reply to SMS (contactID {contactId}) {datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')}" if text==None else text)
+                payload['userId'] = contactId
+        elif endpoint == 'createTask':
+            endpoint_url = f'contacts/{contactId}/tasks'
+            request_type = 'POST'
+            if payload == None:
+                payload = {}
+                payload['title'] = f'Send message to contact {contactId}'
+                payload['body'] = text if text else f"Test task via GHL API at {datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')} Pacific time"
+            payload['dueDate'] = payload.get('dueDate', datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'))
+            payload['completed'] = False
+        elif endpoint == 'sendMessage':
+            endpoint_url = f'conversations/messages'
+            request_type = 'POST'
+            payload["contactId"] = contactId
+            if payload['type'] == 'Email':
+                payload['html'] = payload['message']
+        elif endpoint == 'getEmailHistory':
+            endpoint_url = f'conversations/search?contactId={contactId}'
+            request_type = 'GET'
+            payload = None
+        else:
+            raise ValueError("Invalid endpoint value. Valid values are 'createTask', 'createNote', 'sendMessage', and 'getEmailHistory'.")
 
-    headers = {
-        "Authorization": f"Bearer {token['access_token']}",
-        "Version": "2021-04-15",
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    }
+        url = f'{url_root}{endpoint_url}'
+        try:
+            with open('private/auth_token_response.json') as token_file:
+                token = json.load(token_file)[location]
+        except:
+            try:
+                s3 = boto3.client('s3')
+                response = s3.get_object(Bucket='ownitfit-silvhua', Key='auth_token_response.json')
+                print(f'Token reponse: {response}')
+                token = json.loads(response['Body'].read().decode('utf-8'))
+                print(f"Token: {json.loads(response['Body'].read().decode('utf-8'))}")
+            except Exception as error:
+                exc_type, exc_obj, tb = sys.exc_info()
+                f = tb.tb_frame
+                lineno = tb.tb_lineno
+                filename = f.f_code.co_filename
+                print(f'Error in line {lineno} of {filename}: {str(error)}')
+                return None
+
+        headers = {
+            "Authorization": f"Bearer {token[location]['access_token']}",
+            "Version": "2021-04-15",
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
 
 
-    if request_type == 'POST':
-        response = requests.post(
-            url, headers=headers, 
-            json=payload if payload else None
-        )
-    elif request_type == 'GET':
-        response = requests.get(
-            url, headers=headers, 
-            json=payload if payload else None
-        )
-    else:
-        raise ValueError("Invalid request type. Valid values are 'POST' and 'GET'.")
+        if request_type == 'POST':
+            response = requests.post(
+                url, headers=headers, 
+                json=payload if payload else None
+            )
+        elif request_type == 'GET':
+            response = requests.get(
+                url, headers=headers, 
+                json=payload if payload else None
+            )
+        else:
+            raise ValueError("Invalid request type. Valid values are 'POST' and 'GET'.")
 
-    print(f'Status code {response.status_code}: {response.reason}')
-    data = response.json()
-    data['status_code'] = response.status_code
-    data['response_reason'] = response.reason
-    try:
-        if endpoint == 'getEmailHistory':
-            email_timestamp = data['conversations'][0]['dateUpdated']/1000
-            utc_time = datetime.utcfromtimestamp(email_timestamp)
-            pacific_time = utc_time.replace(tzinfo=pytz.utc).astimezone(pytz.timezone('US/Pacific'))
-            email_timestamp_str = pacific_time.strftime('%Y-%m-%d %H:%M:%S')
+        print(f'Status code {response.status_code}: {response.reason}')
+        data = response.json()
+        data['status_code'] = response.status_code
+        data['response_reason'] = response.reason
+        try:
+            if endpoint == 'getEmailHistory':
+                email_timestamp = data['conversations'][0]['dateUpdated']/1000
+                utc_time = datetime.utcfromtimestamp(email_timestamp)
+                pacific_time = utc_time.replace(tzinfo=pytz.utc).astimezone(pytz.timezone('US/Pacific'))
+                email_timestamp_str = pacific_time.strftime('%Y-%m-%d %H:%M:%S')
 
-            print(f'Last email sent: {email_timestamp_str} Pacific time')
-            data['emailTimestamp_pacific'] = email_timestamp_str
+                print(f'Last email sent: {email_timestamp_str} Pacific time')
+                data['emailTimestamp_pacific'] = email_timestamp_str
+        except Exception as error:
+            exc_type, exc_obj, tb = sys.exc_info()
+            f = tb.tb_frame
+            lineno = tb.tb_lineno
+            filename = f.f_code.co_filename
+            print(f'Error in line {lineno} of {filename}: {str(error)}')
+            return '[Chatbot response]'
+        return data
     except Exception as error:
         exc_type, exc_obj, tb = sys.exc_info()
         f = tb.tb_frame
         lineno = tb.tb_lineno
         filename = f.f_code.co_filename
-        print("An error occurred on line", lineno, "in", filename, ":", error)
-    return data
+        print(f'Error in line {lineno} of {filename}: {str(error)}')
+        return '[Chatbot response]'
