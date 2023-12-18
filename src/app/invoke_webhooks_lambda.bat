50952f5e13396bd292edf2794fefa32eb41845d5
silvhua <silvhua@gmail.com>
2023-12-17 23:12:10 -0800

    add scripts back to repo after removing python packages

---
+++ src/app/invoke_webhooks_lambda.bat
@@ -0,0 +1,8 @@
+sam local invoke WebhooksLambda --event events/OutboundMessageTest.json
+sam local invoke WebhooksLambda --event events/ContactTagUpdateTest.json
+sam local invoke WebhooksLambda --event events/InboundMessageTest.json
+sam local invoke WebhooksLambda --event events/InboundMessageTest.json --force-image-build
+sam local invoke ReplyLambda --event events/InboundMessageTest.json
+sam local invoke ReplyLambda --event events/InboundMessageTest.json --force-image-build
+sam local invoke ReplyLambda --event events/InboundMessageFromLambda.json
+sam local invoke ReplyLambda --event events/InboundMessageFromLambda.json --force-image-build
