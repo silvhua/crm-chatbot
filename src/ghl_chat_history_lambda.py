6325aa68091537f8f63e80b545a7569ab8146192
silvhua <silvhua@gmail.com>
2023-12-21 13:30:58 -0800

    Remove chatbot workflow for now

--- src/ghl_chat_history_lambda.py
+++ src/ghl_chat_history_lambda.py
@@ -49,66 +49,61 @@ def lambda_handler(event, context):
                         )
                 else:
                     message = 'Testing data not added to DynamoDB.'
-                try:
-                    if payload['type'] in message_events:
-                        # if (payload['type'] != 'InboundMessage'):
-                        print(f'Webhook type: {payload["type"]}')
-                        message2 = add_to_chat_history(payload)
-                        message = f'{message}\n{message2}'
-                        if payload['type'] == 'InboundMessage':
-                            location =  os.getenv(payload['locationId'])
-                            print(f'Location: {location}') 
-                            if location == 'SAM Lab': ## Update this later to include other businesses
-                                try:
-                                    contact_details = ghl_request(
-                                        contact_id, endpoint='getContact', 
-                                        location=convert_to_pascal_case(location) ## Update this later to include other businesses
-                                        )
-                                    contact_tags = contact_details['contact']['tags']
-                                    contact_tags = [tag.strip('"\'') for tag in contact_tags]
-                                    print(f'Contact tags: \n{contact_tags}')
+                # try:
+                #     if payload['type'] in message_events:
+                #         print(f'Webhook type: {payload["type"]}')
+                #         message2 = add_to_chat_history(payload)
+                #         message = f'{message}\n{message2}'
+                        # if payload['type'] == 'InboundMessage':
+                        #     location =  os.getenv(payload['locationId'])
+                        #     print(f'Location: {location}') 
+                        #     if location == 'SAM Lab': ## Update this later to include other businesses
+                        #         try:
+                        #             contact_details = ghl_request(
+                        #                 contact_id, endpoint='getContact', 
+                        #                 location=convert_to_pascal_case(location) ## Update this later to include other businesses
+                        #                 )
+                        #             contact_tags = contact_details['contact']['tags']
+                        #             contact_tags = [tag.strip('"\'') for tag in contact_tags]
+                        #             print(f'Contact tags: \n{contact_tags}')
 
-                                    if 'sam lab members' not in contact_tags:
-                                        new_payload = {key: payload[key] for key in ['contactId', 'userId', 'body', 'locationId', 'noReply'] if key in payload}
-                                        # Invoke another Lambda function
-                                        if payload.get("noReply", False) == False:
-                                            try:
-                                                lambda_client = boto3.client('lambda')  # Initialize Lambda client
-                                            except:
-                                                aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
-                                                aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
-                                                region = os.environ.get('AWS_REGION')
-                                                lambda_client = boto3.client(
-                                                    'lambda', region_name=region, 
-                                                    aws_access_key_id=aws_access_key_id, 
-                                                    aws_secret_access_key=aws_secret_access_key
-                                                    )
-                                            lambda_client.invoke(
-                                                FunctionName=os.environ.get('ghl_reply_lambda','ghl_reply'),
-                                                InvocationType='Event',
-                                                Payload=json.dumps(new_payload)
-                                            )
-                                            message3 = f'`ghl_reply` function invoked.'
-                                            message = f'{message}\n{message3}'
-                                        else:
-                                            message3 = f'`ghl_reply` function skipped because `noReply` is set.'
-                                            message = f'{message}\n{message3}'
-                                    else:
-                                        print(f'Contact is a already member. No AI response required.')
-                                except:
-                                    print(f'Error getting contact details.')
-                            # else:
-                            #     print(f'Webhook type: {payload["type"]} for other location')
-                            #     message2 = add_to_chat_history(payload)
-                            #     message = f'{message}\n{message2}'
+                        #             if 'sam lab members' not in contact_tags:
+                        #                 new_payload = {key: payload[key] for key in ['contactId', 'userId', 'body', 'locationId', 'noReply'] if key in payload}
+                        #                 # Invoke another Lambda function
+                        #                 if payload.get("noReply", False) == False:
+                        #                     try:
+                        #                         lambda_client = boto3.client('lambda')  # Initialize Lambda client
+                        #                     except:
+                        #                         aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
+                        #                         aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+                        #                         region = os.environ.get('AWS_REGION')
+                        #                         lambda_client = boto3.client(
+                        #                             'lambda', region_name=region, 
+                        #                             aws_access_key_id=aws_access_key_id, 
+                        #                             aws_secret_access_key=aws_secret_access_key
+                        #                             )
+                        #                     lambda_client.invoke(
+                        #                         FunctionName=os.environ.get('ghl_reply_lambda','ghl_reply'),
+                        #                         InvocationType='Event',
+                        #                         Payload=json.dumps(new_payload)
+                        #                     )
+                        #                     message3 = f'`ghl_reply` function invoked.'
+                        #                     message = f'{message}\n{message3}'
+                        #                 else:
+                        #                     message3 = f'`ghl_reply` function skipped because `noReply` is set.'
+                        #                     message = f'{message}\n{message3}'
+                        #             else:
+                        #                 print(f'Contact is a already member. No AI response required.')
+                        #         except:
+                        #             print(f'Error getting contact details.')
 
-                except Exception as error:
-                    exc_type, exc_obj, tb = sys.exc_info()
-                    f = tb.tb_frame
-                    lineno = tb.tb_lineno
-                    filename = f.f_code.co_filename
-                    message2 = f'An error occurred on line {lineno} in {filename}: {error}.'
-                    message = f'{message}\n{message2}'
+                # except Exception as error:
+                #     exc_type, exc_obj, tb = sys.exc_info()
+                #     f = tb.tb_frame
+                #     lineno = tb.tb_lineno
+                #     filename = f.f_code.co_filename
+                #     message2 = f'An error occurred on line {lineno} in {filename}: {error}.'
+                #     message = f'{message}\n{message2}'
             else:
                 message = f'Contact not in database. No need to save for webhook type {payload["type"]}.'
             print(message)
