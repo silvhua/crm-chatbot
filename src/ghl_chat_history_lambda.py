6c4a9ef3b61ae4235823b0ef264e9118b2737d76
silvhua <silvhua@gmail.com>
2024-01-11 23:20:09 -0800

    temporarily stop invoking ghl_reply lambda

--- src/ghl_chat_history_lambda.py
+++ src/ghl_chat_history_lambda.py
@@ -61,63 +61,63 @@ def lambda_handler(event, context):
                         print(f'Webhook type: {payload["type"]}')
                         message2 = add_to_chat_history(payload)
                         message = f'{message}\n{message2}'
-                        if payload['type'] == 'InboundMessage':
-                            if event.get('direct_local_invoke', None):
-                                location = 'Coach Mcloone'
-                            else:
-                                location =  os.getenv(payload['locationId'])
-                            print(f'Location: {location}') 
-                            if location == 'Coach Mcloone': ## Update this later to include other businesses
-                                try:
-                                    contact_details = ghl_request(
-                                        contact_id, endpoint='getContact', 
-                                        location=convert_to_pascal_case(location) ## Update this later to include other businesses
-                                        )
-                                    contact_tags = contact_details['contact']['tags']
-                                    contact_tags = [tag.strip('"\'') for tag in contact_tags]
-                                    print(f'Contact tags: \n{contact_tags}')
+                        # if payload['type'] == 'InboundMessage':
+                        #     if event.get('direct_local_invoke', None):
+                        #         location = 'Coach Mcloone'
+                        #     else:
+                        #         location =  os.getenv(payload['locationId'])
+                        #     print(f'Location: {location}') 
+                        #     if location == 'Coach Mcloone': ## Update this later to include other businesses
+                        #         try:
+                        #             contact_details = ghl_request(
+                        #                 contact_id, endpoint='getContact', 
+                        #                 location=convert_to_pascal_case(location) ## Update this later to include other businesses
+                        #                 )
+                        #             contact_tags = contact_details['contact']['tags']
+                        #             contact_tags = [tag.strip('"\'') for tag in contact_tags]
+                        #             print(f'Contact tags: \n{contact_tags}')
 
-                                    if 'client' not in contact_tags:
-                                        # new_payload = {key: payload[key] for key in ['contactId', 'userId', 'body', 'locationId', 'noReply'] if key in payload}
-                                        new_payload = payload
-                                        # Invoke another Lambda function
-                                        if payload.get("noReply", False) == False:
-                                            try:
-                                                lambda_client = boto3.client('lambda')  # Initialize Lambda client
-                                            except:
-                                                aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
-                                                aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
-                                                region = os.environ.get('AWS_REGION')
-                                                lambda_client = boto3.client(
-                                                    'lambda', region_name=region, 
-                                                    aws_access_key_id=aws_access_key_id, 
-                                                    aws_secret_access_key=aws_secret_access_key
-                                                    )
-                                            lambda_client.invoke(
-                                                FunctionName=os.environ.get('ghl_reply_lambda','ghl-chat-prod-ReplyLambda-9oAzGMbcYxXB'),
-                                                InvocationType='Event',
-                                                Payload=json.dumps(new_payload)
-                                            )
-                                            message3 = f'`ghl_reply` function invoked.'
-                                            message = f'{message}\n{message3}'
-                                        else:
-                                            message3 = f'`ghl_reply` function skipped because `noReply` is set.'
-                                            message = f'{message}\n{message3}'
-                                    else:
-                                        print(f'Contact is a already member. No AI response required.')
-                                except Exception as error:
-                                    exc_type, exc_obj, tb = sys.exc_info()
-                                    f = tb.tb_frame
-                                    lineno = tb.tb_lineno
-                                    filename = f.f_code.co_filename
-                                    message3 = f'Error getting contact details. An error occurred on line {lineno} in {filename}: {error}.'
-                                    message = f'{message}\n{message3}'
-                            else:
-                                message4 = f'Not an inbound message; ghl_reply skipped.'
-                                message = f'{message}\n{message4}'
-                        else:
-                            message4 = f'No location found; ghl_reply skipped.'
-                            message = f'{message}\n{message4}'
+                        #             if 'client' not in contact_tags:
+                        #                 # new_payload = {key: payload[key] for key in ['contactId', 'userId', 'body', 'locationId', 'noReply'] if key in payload}
+                        #                 new_payload = payload
+                        #                 # Invoke another Lambda function
+                        #                 if payload.get("noReply", False) == False:
+                        #                     try:
+                        #                         lambda_client = boto3.client('lambda')  # Initialize Lambda client
+                        #                     except:
+                        #                         aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
+                        #                         aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+                        #                         region = os.environ.get('AWS_REGION')
+                        #                         lambda_client = boto3.client(
+                        #                             'lambda', region_name=region, 
+                        #                             aws_access_key_id=aws_access_key_id, 
+                        #                             aws_secret_access_key=aws_secret_access_key
+                        #                             )
+                        #                     lambda_client.invoke(
+                        #                         FunctionName=os.environ.get('ghl_reply_lambda','ghl-chat-prod-ReplyLambda-9oAzGMbcYxXB'),
+                        #                         InvocationType='Event',
+                        #                         Payload=json.dumps(new_payload)
+                        #                     )
+                        #                     message3 = f'`ghl_reply` function invoked.'
+                        #                     message = f'{message}\n{message3}'
+                        #                 else:
+                        #                     message3 = f'`ghl_reply` function skipped because `noReply` is set.'
+                        #                     message = f'{message}\n{message3}'
+                        #             else:
+                        #                 print(f'Contact is a already member. No AI response required.')
+                        #         except Exception as error:
+                        #             exc_type, exc_obj, tb = sys.exc_info()
+                        #             f = tb.tb_frame
+                        #             lineno = tb.tb_lineno
+                        #             filename = f.f_code.co_filename
+                        #             message3 = f'Error getting contact details. An error occurred on line {lineno} in {filename}: {error}.'
+                        #             message = f'{message}\n{message3}'
+                        #     else:
+                        #         message4 = f'Not an inbound message; ghl_reply skipped.'
+                        #         message = f'{message}\n{message4}'
+                        # else:
+                        #     message4 = f'No location found; ghl_reply skipped.'
+                        #     message = f'{message}\n{message4}'
 
                 except Exception as error:
                     exc_type, exc_obj, tb = sys.exc_info()
