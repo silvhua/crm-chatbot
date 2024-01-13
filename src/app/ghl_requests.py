83036d181df252a59b2ca6099e1b108938492f53
silvhua <silvhua@gmail.com>
2024-01-13 05:33:55 -0800

    use environmental variables rather than config.json

--- src/app/ghl_requests.py
+++ src/app/ghl_requests.py
@@ -1,12 +1,17 @@
 # import pytz # Not included in Lambda by default
 import sys
+import os
 import requests
 from datetime import datetime, timedelta
 import json
 from urllib.parse import urlencode
 import boto3
 import random
-# from pprint import pprint
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except:
+    pass
 
 def refresh_token(location='CoachMcloone', token_file_path = 'app/private'):
     """
@@ -33,11 +38,13 @@ def refresh_token(location='CoachMcloone', token_file_path = 'app/private'):
 
     filename = 'auth_token_response_cicd.json'
     # filename = 'auth_token_response.json' # Original
-    config_file_name = 'config.json'
-    # config_file_name = 'config_cicid_app.json' # cicd
-
-    with open(f'{token_file_path}/{config_file_name}') as config_file:
-        appConfig = json.load(config_file)
+    # config_file_name = 'config.json'
+    # with open(f'{token_file_path}/{config_file_name}') as config_file:
+    #     appConfig = json.load(config_file)
+    appConfig = dict()
+    appConfig["clientId"] = os.environ["clientId"]
+    appConfig["clientSecret"] = os.environ["clientSecret"]
+    
     s3 = boto3.client('s3')
     response = s3.get_object(Bucket='ownitfit-silvhua', Key=filename)
     tokens = json.loads(response['Body'].read().decode('utf-8'))
