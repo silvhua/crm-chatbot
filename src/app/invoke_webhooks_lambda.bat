f3857640fabebb75dfbaa15a83db9ae2ac604033
silvhua <silvhua@gmail.com>
2024-01-13 05:33:20 -0800

    remove old command

--- src/app/invoke_webhooks_lambda.bat
+++ src/app/invoke_webhooks_lambda.bat
@@ -2,11 +2,12 @@ source activate-ghl.sh
 sam local invoke WebhooksLambda --event events/OutboundMessageTest.json
 sam local invoke WebhooksLambda --event events/ContactTagUpdateTest.json
 sam local invoke WebhooksLambda --event events/InboundMessageTest.json
+sam local invoke WebhooksLambda --event events/InboundMessage.json
 sam local invoke WebhooksLambda --event events/InboundMessageTest.json --force-image-build
 sam local invoke ReplyLambda --event events/InboundMessageTest.json
 sam local invoke ReplyLambda --event events/InboundMessageTest.json --force-image-build
 sam local invoke ReplyLambda --event events/InboundMessageFromLambda.json
 sam local invoke ReplyLambda --event events/InboundMessageFromLambda.json --force-image-build
-sam local invoke WebhooksLambda --event events/CustomInboundMessage.json
-sam local invoke WebhooksLambda --event events/CustomInboundMessageTest.json
-sam local invoke WebhooksLambda --event events/CustomInboundMessage.json
+@REM sam local invoke WebhooksLambda --event events/CustomInboundMessage.json
+@REM sam local invoke WebhooksLambda --event events/CustomInboundMessageTest.json
+@REM sam local invoke WebhooksLambda --event events/CustomInboundMessage.json
