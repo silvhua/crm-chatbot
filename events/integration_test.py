716e5d8032e199e29a43b5c6d2e6d48ee59e2dd2
silvhua <silvhua@gmail.com>
2024-01-13 08:09:18 -0800

    use `ghl_reply_lambda` for integration test

---
+++ events/integration_test.py
@@ -0,0 +1,42 @@
+import json
+import sys
+sys.path.append('../')
+sys.path.append('../src')
+sys.path.append('../src/app')
+from src import ghl_reply_lambda
+import pytest
+from app import data_functions
+from datetime import datetime, timezone
+import os
+
+def load_json(filename, filepath):
+    """
+    Load a JSON file using specified file path copied from windows file explorer.
+    Back slashes in file path will be converted to forward slashes.
+
+    Arguments:
+    - filepath (raw string): Use the format r'<path>'.
+    - filename (string).
+    """
+    filename = f'{filepath}/'.replace('\\','/')+filename
+    with open(filename) as file:
+        return json.load(file)
+
+@pytest.fixture()
+def apigw_event():
+    job = os.environ.get('JOB_NAME')
+    if job == 'test':
+        test = 'ContactCreateTest.json'
+    else:
+        test = 'InboundMessageTest.json'
+    result =  load_json(test, '.')
+    timestamp = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
+    result['dateAdded'] = timestamp
+    return result
+
+def test_lambda_handler(apigw_event):
+
+    ret = ghl_reply_lambda.lambda_handler(apigw_event, "")
+    data = json.loads(ret["body"])
+
+    assert ret["statusCode"] == 200, data
