2eada3e4b63f783afed93080ccb701b75082be36
silvhua <silvhua@gmail.com>
2024-01-31 17:48:26 -0800

    `from langchain_community.chat_message_histories import DynamoDBChatMessageHistory`

--- src/app/data_functions.py
+++ src/app/data_functions.py
@@ -205,7 +205,7 @@ def add_webhook_data_to_dynamodb(payload, table_name, dynamodb):
 
 def add_to_chat_history(payload):
     try:
-        from langchain.memory.chat_message_histories import DynamoDBChatMessageHistory
+        from langchain_community.chat_message_histories import DynamoDBChatMessageHistory
         contactId = payload.get('contactId', 'no contact id')
 
         history = DynamoDBChatMessageHistory(
