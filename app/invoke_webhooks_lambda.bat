b93fec4f135106c9aca17fcf74159d584211863a
silvhua <silvhua@gmail.com>
2024-02-28 15:45:08 -0800

    update event JSON path

--- app/invoke_webhooks_lambda.bat
+++ app/invoke_webhooks_lambda.bat
@@ -1,20 +1,20 @@
 source activate-ghl.sh
-sam local invoke WebhooksLambda --event events/OutboundMessageTest.json
-sam local invoke WebhooksLambda --event events/ContactTagUpdateTest.json
-sam local invoke WebhooksLambda --event events/InboundMessageTest.json
-sam local invoke WebhooksLambda --event events/InboundMessage.json
-sam local invoke WebhooksLambda --event events/InboundMessageTest.json --force-image-build
-sam local invoke WebhooksLambda --event events/EmailOutboundMessage.json
+sam local invoke WebhooksLambda --event src/events/OutboundMessageTest.json
+sam local invoke WebhooksLambda --event src/events/ContactTagUpdateTest.json
+sam local invoke WebhooksLambda --event src/events/InboundMessageTest.json
+sam local invoke WebhooksLambda --event src/events/InboundMessage.json
+sam local invoke WebhooksLambda --event src/events/InboundMessageTest.json --force-image-build
+sam local invoke WebhooksLambda --event src/events/EmailOutboundMessage.json
 
 
-sam local invoke ReplyLambda --event events/InboundMessage.json
-sam local invoke ReplyLambda --event events/InboundMessageTest.json
-sam local invoke ReplyLambda --event events/InboundMessageTest.json --force-image-build
+sam local invoke ReplyLambda --event src/events/InboundMessage.json
+sam local invoke ReplyLambda --event src/events/InboundMessageTest.json
+sam local invoke ReplyLambda --event src/events/InboundMessageTest.json --force-image-build
 @REM sam local invoke ReplyLambda --event events/InboundMessageFromLambda.json
 @REM sam local invoke ReplyLambda --event events/InboundMessageFromLambda.json --force-image-build
 
 
-sam local invoke FollowupLambda --event events/followupWorkflowWebhook.json
+sam local invoke FollowupLambda --event src/events/followupWorkflowWebhook.json
 
 @REM sam local invoke WebhooksLambda --event events/CustomInboundMessage.json
 @REM sam local invoke WebhooksLambda --event events/CustomInboundMessageTest.json
