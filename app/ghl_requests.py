134cc524c0684fab09050eb251cc675ded724ef8
silvhua <silvhua@gmail.com>
2024-03-17 21:50:24 -0700

    created `Ghl` class; made `ghl_request` a method, renamed to `.send_request`

--- app/ghl_requests.py
+++ app/ghl_requests.py
@@ -7,6 +7,7 @@ import json
 from urllib.parse import urlencode
 import boto3
 import random
+from app.Custom_Logger import *
 try:
     from dotenv import load_dotenv
     load_dotenv()
@@ -107,145 +108,14 @@ def refresh_token(location='CoachMcloone', token_file_path = 'app/private'):
             'response': json.dumps(response.json())
         }
 
-def ghl_request(
-        contactId, endpoint='createTask', text=None, payload=None, location='CoachMcloone', 
-        path_param=None, locationId=None, params_dict=None
-        ):
-    """
-    Send a message to a contact in GoHighLevel or retrieve email history.
-
-    Parameters:
-    - contactId (str): Contact ID OR locationId if endpoint is 'getWorkflow'.
-    - endpoint (str): API endpoint. Valid values are 'createTask', 'workflow', 'getWorkflow', \
-        'createNote', 'send_message', 'getContacts', 'updateContact', \
-        'searchConversations', 'searchUsers' \
-        'getLocation', 'addTag', 'removeTag', and 'getEmailHistory'.
-    - payload (dict): Dictionary containing the payload for the request.
-    - params_dict (dict): Dictionary containing additional parameters for the request.
-    - location (str): Location value for retrieving the authentication token.
-    - path_param (str): Additional path parameter for the request.
-    - locationId: locationId for getContacts endpoint.
-
-    Example payload for sendMessage endpoint:
-        
-            payload = {
-                "type": "Email",
-                "message": f"Hi, me. This is a test message from the GHL API using Python at {timestamp} Pacific time",
-                "subject": "Testing GHL API with Python threadId",
-                "emailFrom": "Brian Quinlan <brian@ownitfit.com.au>",
-                "threadId": 'Y0ecBIIPiHa7bpZr616G'
-            }
-
-    Returns:
-    - response_dict (dict): Dictionary containing the response from the API.
-    """
-    url_root = 'https://services.leadconnectorhq.com/'
-    data = {}
-    if payload:
-        print(f'input payload: {payload}')
-    try:
-        if endpoint == 'getContact':
-            endpoint_url = f'contacts/{contactId}'
-            request_type = 'GET'
-            payload = None
-            params = None
-        elif endpoint == 'createTask':
-            endpoint_url = f'contacts/{contactId}/tasks'
-            request_type = 'POST'
-            if payload == None:
-                payload = {}
-                payload['title'] = ''
-                if params_dict:
-                    payload['title'] += f'{"Human attention needed: " if params_dict["alert_human"]==True else "Chatbot response: "}'
-                    payload['body'] = params_dict['response']
-                else:
-                    payload['body'] = text if text else f"Test task via GHL API at {datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')} UTC time"
-                payload['title'] += f'Send message to contact {contactId}{" "+text if text else ""}.'
-                payload['assignedTo'] = os.environ['user_id'] if contactId != os.environ['my_contact_id'] else None
-                if params_dict:
-                    if params_dict.get('phone_number') != None:
-                        payload['title'] += f' Phone number: {params_dict["phone_number"]}'
-            payload['dueDate'] = payload[3] if len(payload) > 3 else datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
-            payload['completed'] = False
-        elif endpoint == 'workflow':
-            endpoint_url = f'contacts/{contactId}/workflow/{path_param}'
-            request_type = 'POST'
-            payload = {}
-            payload['eventStartTime'] = (datetime.utcnow() + timedelta(minutes=random.randint(2, 10))).strftime('%Y-%m-%dT%H:%M:%S+00:00')
-        elif endpoint == 'removeFromWorkflow':
-            endpoint_url = f'contacts/{contactId}/workflow/{path_param}'
-            request_type = 'DELETE'
-            payload = {}
-        elif endpoint == 'getWorkflow': ### 
-            endpoint_url = r'workflows/'
-            request_type = 'GET'
-            params = {'locationId': contactId if contactId else locationId}
-        elif endpoint == 'createNote':
-            endpoint_url = f'contacts/{contactId}/notes'
-            request_type = 'POST'
-            if payload == None:
-                payload = {}
-                payload['body'] = (f"Reply to SMS (contactID {contactId}) {datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')}" if text==None else text)
-                payload['userId'] = contactId
-        elif endpoint == 'sendMessage':
-            endpoint_url = f'conversations/messages'
-            request_type = 'POST'
-            payload["contactId"] = contactId
-            if payload['type'] == 'Email':
-                payload['html'] = payload['message']
-        elif endpoint == 'getEmailHistory':
-            endpoint_url = f'conversations/search?contactId={contactId}'
-            request_type = 'GET'
-            payload = None
-        elif (endpoint == 'getContacts') | (endpoint == 'searchConversations') | (endpoint == 'searchUsers') | (endpoint == 'getLocation'): 
-            if endpoint=='searchConversations':
-                endpoint_url = f'conversations/search'
-            elif endpoint == 'searchUsers':
-                endpoint_url = f'users/search'            
-            elif endpoint == 'getContacts':
-                endpoint_url = f'contacts/'
-            else:
-                endpoint_url = f'locations/{locationId}'
-            request_type = 'GET'
-            payload = None
-            if params_dict:
-                params = params_dict
-            else:
-                params = {
-                    'locationId': locationId,
-                    'query': contactId
-                }
-        elif endpoint == 'addTag':
-            endpoint_url = f'contacts/{contactId}/tags'
-            request_type = 'POST'
-            if payload == None:
-                tags = text if type(text) == list else [text] 
-                payload = {
-                    "tags": tags
-                }
-        elif endpoint == 'removeTag':
-            endpoint_url = f'contacts/{contactId}/tags'
-            request_type = 'DELETE'
-            if payload == None:
-                tags = text if type(text) == list else [text] 
-                payload = {
-                    "tags": tags
-                }
-        elif endpoint == 'updateContact':
-            endpoint_url = f'contacts/{contactId}'
-            request_type = 'PUT'
-        elif endpoint == 'getConversation':
-            endpoint_url = f'conversations/{contactId if contactId else path_param}'
-            request_type = 'GET'
-        else:
-            raise ValueError("Invalid endpoint value. Valid values are 'createTask', 'createNote', 'sendMessage', and 'getEmailHistory'.")
-
-        url = f'{url_root}{endpoint_url}'
+class Ghl:
+    def __init__(self, location):
+        self.logger = Custom_Logger(__name__, level=logging.DEBUG)
         token_filename = 'auth_token_response_cicd.json'
         try:
             s3 = boto3.client('s3')
             response = s3.get_object(Bucket='ownitfit-silvhua', Key=token_filename)
-            token = json.loads(response['Body'].read().decode('utf-8'))[location]
+            self.token = json.loads(response['Body'].read().decode('utf-8'))[location]
         except Exception as error:
             exc_type, exc_obj, tb = sys.exc_info()
             f = tb.tb_frame
@@ -253,65 +123,199 @@ def ghl_request(
             filename = f.f_code.co_filename
             message = f'[ERROR] Error in line {lineno} of {filename}: {str(error)}'
             print(message)
-            return data
+            
+    def send_request(self,
+            contactId, endpoint='createTask', text=None, payload=None, location='CoachMcloone', 
+            path_param=None, locationId=None, params_dict=None
+            ):
+        """
+        Send a message to a contact in GoHighLevel or retrieve email history.
 
-        headers = {
-            "Authorization": f"Bearer {token['access_token']}",
-            "Version": "2021-07-28",
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        }
+        Parameters:
+        - contactId (str): Contact ID OR locationId if endpoint is 'getWorkflow'.
+        - endpoint (str): API endpoint. Valid values are 'createTask', 'workflow', 'getWorkflow', \
+            'createNote', 'send_message', 'getContacts', 'updateContact', \
+            'searchConversations', 'searchUsers' \
+            'getLocation', 'addTag', 'removeTag', and 'getEmailHistory'.
+        - payload (dict): Dictionary containing the payload for the request.
+        - params_dict (dict): Dictionary containing additional parameters for the request.
+        - location (str): Location value for retrieving the authentication token.
+        - path_param (str): Additional path parameter for the request.
+        - locationId: locationId for getContacts endpoint.
 
-        if request_type in 'POST':
-            response = requests.post(
-                url, headers=headers, 
-                json=payload if payload else None
-            )
-        elif request_type == 'GET':
-            response = requests.get(
-                url, headers=headers, 
-                json=payload if payload else None,
-                params=params if params else None
-            )
-        elif request_type == 'DELETE':
-            response = requests.delete(
-                url, headers=headers,
-                json=payload if payload else None
-            )
-        elif request_type == 'PUT':
-            response = requests.put(
-                url, headers=headers,
-                json=payload if payload else None
-            )
-        else:
-            raise ValueError("Invalid request type. Valid values are 'POST', 'GET', 'DELETE' and 'PUT'.")
+        Example payload for sendMessage endpoint:
+            
+                payload = {
+                    "type": "Email",
+                    "message": f"Hi, me. This is a test message from the GHL API using Python at {timestamp} Pacific time",
+                    "subject": "Testing GHL API with Python threadId",
+                    "emailFrom": "Brian Quinlan <brian@ownitfit.com.au>",
+                    "threadId": 'Y0ecBIIPiHa7bpZr616G'
+                }
 
-        print(f'GHL request status code for `{endpoint}` endpoint: {response.status_code}: {response.reason}')
-        data = response.json()
-        data['status_code'] = response.status_code
-        data['response_reason'] = response.reason
+        Returns:
+        - response_dict (dict): Dictionary containing the response from the API.
+        """
+        url_root = 'https://services.leadconnectorhq.com/'
+        data = {}
+        if payload:
+            print(f'input payload: {payload}')
         try:
-            if endpoint == 'getEmailHistory':
-                email_timestamp = data['conversations'][0]['dateUpdated']/1000
-                utc_time = datetime.utcfromtimestamp(email_timestamp)
-                pacific_time = utc_time.replace(tzinfo=pytz.utc).astimezone(pytz.timezone('US/Pacific'))
-                email_timestamp_str = pacific_time.strftime('%Y-%m-%d %H:%M:%S')
+            if endpoint == 'getContact':
+                endpoint_url = f'contacts/{contactId}'
+                request_type = 'GET'
+                payload = None
+                params = None
+            elif endpoint == 'createTask':
+                endpoint_url = f'contacts/{contactId}/tasks'
+                request_type = 'POST'
+                if payload == None:
+                    payload = {}
+                    payload['title'] = ''
+                    if params_dict:
+                        payload['title'] += f'{"Human attention needed: " if params_dict["alert_human"]==True else "Chatbot response: "}'
+                        payload['body'] = params_dict['response']
+                    else:
+                        payload['body'] = text if text else f"Test task via GHL API at {datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')} UTC time"
+                    payload['title'] += f'Send message to contact {contactId}{" "+text if text else ""}.'
+                    payload['assignedTo'] = os.environ['user_id'] if contactId != os.environ['my_contact_id'] else None
+                    if params_dict:
+                        if params_dict.get('phone_number') != None:
+                            payload['title'] += f' Phone number: {params_dict["phone_number"]}'
+                payload['dueDate'] = payload[3] if len(payload) > 3 else datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+                payload['completed'] = False
+            elif endpoint == 'workflow':
+                endpoint_url = f'contacts/{contactId}/workflow/{path_param}'
+                request_type = 'POST'
+                payload = {}
+                payload['eventStartTime'] = (datetime.utcnow() + timedelta(minutes=random.randint(2, 10))).strftime('%Y-%m-%dT%H:%M:%S+00:00')
+            elif endpoint == 'removeFromWorkflow':
+                endpoint_url = f'contacts/{contactId}/workflow/{path_param}'
+                request_type = 'DELETE'
+                payload = {}
+            elif endpoint == 'getWorkflow': ### 
+                endpoint_url = r'workflows/'
+                request_type = 'GET'
+                params = {'locationId': contactId if contactId else locationId}
+            elif endpoint == 'createNote':
+                endpoint_url = f'contacts/{contactId}/notes'
+                request_type = 'POST'
+                if payload == None:
+                    payload = {}
+                    payload['body'] = (f"Reply to SMS (contactID {contactId}) {datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')}" if text==None else text)
+                    payload['userId'] = contactId
+            elif endpoint == 'sendMessage':
+                endpoint_url = f'conversations/messages'
+                request_type = 'POST'
+                payload["contactId"] = contactId
+                if payload['type'] == 'Email':
+                    payload['html'] = payload['message']
+            elif endpoint == 'getEmailHistory':
+                endpoint_url = f'conversations/search?contactId={contactId}'
+                request_type = 'GET'
+                payload = None
+            elif (endpoint == 'getContacts') | (endpoint == 'searchConversations') | (endpoint == 'searchUsers') | (endpoint == 'getLocation'): 
+                if endpoint=='searchConversations':
+                    endpoint_url = f'conversations/search'
+                elif endpoint == 'searchUsers':
+                    endpoint_url = f'users/search'            
+                elif endpoint == 'getContacts':
+                    endpoint_url = f'contacts/'
+                else:
+                    endpoint_url = f'locations/{locationId}'
+                request_type = 'GET'
+                payload = None
+                if params_dict:
+                    params = params_dict
+                else:
+                    params = {
+                        'locationId': locationId,
+                        'query': contactId
+                    }
+            elif endpoint == 'addTag':
+                endpoint_url = f'contacts/{contactId}/tags'
+                request_type = 'POST'
+                if payload == None:
+                    tags = text if type(text) == list else [text] 
+                    payload = {
+                        "tags": tags
+                    }
+            elif endpoint == 'removeTag':
+                endpoint_url = f'contacts/{contactId}/tags'
+                request_type = 'DELETE'
+                if payload == None:
+                    tags = text if type(text) == list else [text] 
+                    payload = {
+                        "tags": tags
+                    }
+            elif endpoint == 'updateContact':
+                endpoint_url = f'contacts/{contactId}'
+                request_type = 'PUT'
+            elif endpoint == 'getConversation':
+                endpoint_url = f'conversations/{contactId if contactId else path_param}'
+                request_type = 'GET'
+            else:
+                raise ValueError("Invalid endpoint value. Valid values are 'createTask', 'createNote', 'sendMessage', and 'getEmailHistory'.")
 
-                print(f'Last email sent: {email_timestamp_str} Pacific time')
-                data['emailTimestamp_pacific'] = email_timestamp_str
+            url = f'{url_root}{endpoint_url}'
+
+            headers = {
+                "Authorization": f"Bearer {self.token['access_token']}",
+                "Version": "2021-07-28",
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+
+            if request_type in 'POST':
+                response = requests.post(
+                    url, headers=headers, 
+                    json=payload if payload else None
+                )
+            elif request_type == 'GET':
+                response = requests.get(
+                    url, headers=headers, 
+                    json=payload if payload else None,
+                    params=params if params else None
+                )
+            elif request_type == 'DELETE':
+                response = requests.delete(
+                    url, headers=headers,
+                    json=payload if payload else None
+                )
+            elif request_type == 'PUT':
+                response = requests.put(
+                    url, headers=headers,
+                    json=payload if payload else None
+                )
+            else:
+                raise ValueError("Invalid request type. Valid values are 'POST', 'GET', 'DELETE' and 'PUT'.")
+
+            print(f'GHL request status code for `{endpoint}` endpoint: {response.status_code}: {response.reason}')
+            data = response.json()
+            data['status_code'] = response.status_code
+            data['response_reason'] = response.reason
+            try:
+                if endpoint == 'getEmailHistory':
+                    email_timestamp = data['conversations'][0]['dateUpdated']/1000
+                    utc_time = datetime.utcfromtimestamp(email_timestamp)
+                    pacific_time = utc_time.replace(tzinfo=pytz.utc).astimezone(pytz.timezone('US/Pacific'))
+                    email_timestamp_str = pacific_time.strftime('%Y-%m-%d %H:%M:%S')
+
+                    print(f'Last email sent: {email_timestamp_str} Pacific time')
+                    data['emailTimestamp_pacific'] = email_timestamp_str
+            except Exception as error:
+                exc_type, exc_obj, tb = sys.exc_info()
+                f = tb.tb_frame
+                lineno = tb.tb_lineno
+                filename = f.f_code.co_filename
+                print(f'Error in line {lineno} of {filename}: {str(error)}')
         except Exception as error:
             exc_type, exc_obj, tb = sys.exc_info()
             f = tb.tb_frame
             lineno = tb.tb_lineno
             filename = f.f_code.co_filename
             print(f'Error in line {lineno} of {filename}: {str(error)}')
-    except Exception as error:
-        exc_type, exc_obj, tb = sys.exc_info()
-        f = tb.tb_frame
-        lineno = tb.tb_lineno
-        filename = f.f_code.co_filename
-        print(f'Error in line {lineno} of {filename}: {str(error)}')
-    return data
+        return data
 
 def parse_result_id(response, result_type):
     """
